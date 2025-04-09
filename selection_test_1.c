ea2756bff493c2a9b0b1690d9a9231200532efe5
Sk-Salman027 <graphicdsalman@gmail.com>
2025-04-09 23:40:42 +0600

    selection test 1

---
+++ selection_test_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // variable declaration
+    int A, B, C, D;
+    // take input
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+    // condition check
+    if ((B > C) && (D > A) && ((C + D) > (A + B)) && D > 0 && A % 2 == 0)
+    {
+        printf("Valores aceitos\n");
+    }
+    else
+    {
+        printf("Valores nao aceitos\n");
+    }
+}
